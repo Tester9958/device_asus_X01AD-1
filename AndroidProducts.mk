6274b9fdaf2437e726ea21938985e4a55518ceef
Tester9958 <103098781+Tester9958@users.noreply.github.com>
2022-05-01 10:58:12 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_X01AD.mk
+    $(LOCAL_DIR)/pa_X01AD.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_X01AD-user \
-    aosp_X01AD-userdebug \
-    aosp_X01AD-eng
+    pa_X01AD-user \
+    pa_X01AD-userdebug \
+    pa_X01AD-eng
